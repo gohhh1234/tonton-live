7dc7bcefd2c50afc7ba8b24be7a346003318c58a
gohhh1234 <55094182+gohhh1234@users.noreply.github.com>
2021-10-30 20:00:12 +0800

    Update download_token.sh

--- download_token.sh
+++ download_token.sh
@@ -7,8 +7,8 @@ echo -e "\e[1;31m [INFO] Downloading TV3 \e[0m"
 TV3="http://wdu2kjrn.dongtaiyuming.net/tonton-live/TV3.php?"$AUTH
 wget -O TV3.m3u8 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31" $TV3
 cp 'TV3.m3u8' 'm3u8/TV3.m3u8'
-sed '/#cell=lsg1/'d TV3.m3u8
 rm 'TV3.m3u8'
+sed '/#cell=lsg1/'d TV3.m3u8
 
 echo -e "\e[1;31m [INFO] Downloading DidikTVKPM \e[0m"
 DidikTVKPM="http://wdu2kjrn.dongtaiyuming.net/tonton-live/didikTVKPM.php?"$AUTH
